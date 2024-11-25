d72c4fcd622c0b3b33f7552a21c5688c02f6f543
ngoc-dan <ngocdan191026@gmail.com>
2024-11-25 11:48:58 +0700

    Add files via upload

---
+++ 5.3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int n,t;
+	printf("nhap so n : ");
+	scanf("%d",&n);
+	for(int i=0; i<=n ;i++){
+		t+=i;
+		}
+	printf("tong cac so n la : %d",t);
+	
+	return 0;
+}
